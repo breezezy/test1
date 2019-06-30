48e5158e4612952dd0019aea62be40c74c363e2b
breezezy <276164500@qq.com>
2019-06-30 23:45:31 +0800

    添加项目文件。

---
+++ 程序流程结构/敲桌子.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+using namespace std;
+
+int main() {
+	int i = 0;
+	for ( i = 1; i <= 100;i++) {
+		if (i % 10 == 7 || i / 10 == 7 || i / 7 == 0) {
+			cout << "敲桌子";
+		}
+		/*else 
+		cout << i << endl;*/
+	}
+	for (i = 1; i <= 100;i++) {
+		cout << i << endl;
+	}
+	return 0;
+}
---
+++ 程序流程结构/猜数字.cpp
@@ -0,0 +1,30 @@
+﻿#include<iostream>
+#include<ctime>
+using namespace std;
+
+int main1() {
+	int num, max = 10,count = 0;
+	srand((unsigned int)time(NULL));
+	int r = rand() % 100 + 1;
+	while ( count < max ) {
+		cin >> num;
+		count++;
+		if (num == r) {
+			
+			cout << "恭喜你，猜了"<<count<<"次就猜对了" << endl;
+			break;
+		}
+		else if (num < r) {
+			cout << "猜小了" << endl;
+		}
+		else {
+			cout << "猜大了" << endl;
+		}
+	}
+	if (count == max) {
+		cout << "很遗憾，" << count << "次机会用完了，你没猜对" << endl;
+	}
+	
+
+	return 0;
+}
